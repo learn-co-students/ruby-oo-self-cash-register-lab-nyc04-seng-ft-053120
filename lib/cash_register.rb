d138b6b728cc43bb8e9fb3fd7dc82b5156ef784f
chrisvsanchez <chrisvsanchez@gmail.com>
2020-06-24 08:56:03 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+require 'pry'
+class CashRegister
+    
+attr_accessor :total, :discount
 
+    def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart =[]
+    @previous_total = 0
+    end
+
+    def add_item(title, price, quantity = 1)
+        @cart << price 
+    #    @cart.reduce(:+) 
+        @cart.each do |price|
+            @total += price * quantity
+        end
+        @previous_total = 
+    end
+    
+end
