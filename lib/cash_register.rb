d0a523423b69c452b82628aa5e592ccef8dbbd13
chrisvsanchez <chrisvsanchez@gmail.com>
2020-06-24 18:30:30 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,12 +11,22 @@ attr_accessor :total, :discount
     end
 
     def add_item(title, price, quantity = 1)
-        @cart << price 
-    #    @cart.reduce(:+) 
-        @cart.each do |price|
-            @total += price * quantity
+        # @cart << price 
+        # @cart.each do |price|
+            @total = @total + price * quantity
+            @cart << title * quantity.times 
         end
-        @previous_total = 
-    end
-    
+        
+        def apply_discount
+            if @discount > 0
+                @total = @total - @total * @discount / 100
+                    return "After the discount, the total comes to $#{@total}."
+                else 
+                    return "There is no discount to apply."
+            end
+        end
+
+        def items
+            @cart
+        end
 end
