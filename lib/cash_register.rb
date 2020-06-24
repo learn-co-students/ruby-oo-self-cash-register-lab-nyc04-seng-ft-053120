bbd83bdb9f43f1eeb5cd0e77a7ab10b7589670b3
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-24 12:42:35 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,24 +1,29 @@
 class CashRegister
+
     def initialize(discount = 0)
         @total = 0
         @discount = discount
         @cart = []
     end
-
+    #getter
     def discount
         @discount
     end
-
+    #getter
     def total
         @total
     end
-
+    #setter
     def total=(total)
         @total = total
     end
 
+    #setter
     def add_item(title, price, quantity = 1)
-
+        quantity.times do
+            @cart << title
+        end
+        @last_price = (price * quantity)
         @total += (price * quantity)
     end
 
@@ -30,8 +35,17 @@ class CashRegister
             return "There is no discount to apply."
         end
     end
-
+    #getter
     def items
-        
+        @cart
+    end
+    
+    def void_last_transaction
+        @cart.delete(-1)
+        @total = @total - @last_price
+    end
+    #getter
+    def last_price
+        @last_price
     end
 end
